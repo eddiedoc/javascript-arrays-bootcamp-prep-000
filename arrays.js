7662d7e3c9a81327d67a902bda84436c4727cee0
eddie <psyck_o@hotmail.com>
2017-12-31 14:20:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['Snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray(array, el) {
-  var array2 = array.unshift(el);
+  var array2 = [el,...array];
   return array2;
 }
 
